aa08f06f4addbca161ea7416ae9b81c60d330ae0
Nathan Caliani <nathan.caliani09@gmail.com>
2025-03-10 19:16:33 -0300

    Conteúdo adicionado
    conteúdo dos exercícios 12 e 13  adicionados

--- Exercicios/ex012.py
+++ Exercicios/ex012.py
@@ -1,2 +1,5 @@
-#
+#Faça um algoritmo que leia um e mostre o preço e calcule um desconte de 5% do produto.
 
+preco = float(input("Qual o preço do produto: "))
+precoDesconto = preco - (preco * 5 / 100)
+print(f"O produto que custava {preco:.2f}, na promoção com desconto de 5% vai custar {precoDesconto:.2f}")
--- Exercicios/ex013.py
+++ Exercicios/ex013.py
@@ -1,2 +1,5 @@
 #
 
+salario = float(input("Qual é o sálario do funcionário: "))
+novoSalario = salario + (salario * 15 / 100)
+print(f"Um funcionário que ganhava {salario:.2f}, com 15% de aumento, passa a receber {novoSalario:.2f}")
