acadddd5d94f4573d6b3e26fcfc57d661abb14f8
Nathan Caliani <nathan.caliani09@gmail.com>
2025-03-11 18:10:35 -0300

    add
    add 19

--- Exercicios0_20/ex019.py
+++ Exercicios0_20/ex019.py
@@ -1,2 +1,10 @@
-#
+#Um professor quer sortear um dos quatro alunos. Faça um programa que ajude ele, lendo o nome deles e
+#escrevendo o nome do escolhido.
 
+from random import choice
+
+aluno1 = input("Primeiro aluno: ")
+aluno2 = input("Segundo aluno: ")
+aluno3 = input("Terceiro aluno: ")
+aluno4 = input("Quarto aluno: ")
+print(f"O aluno escolhido foi {choice([aluno1, aluno2, aluno3, aluno4])}")
