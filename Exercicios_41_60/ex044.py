4acc873fdf45ce23c93151859298ca3727c907d9
Nathan Caliani <nathan.caliani09@gmail.com>
2025-03-14 20:36:55 -0300

    Exercícios add
    conteúdos dos exercícios do 41 a 45 adicionados

--- Exercicios_41_60/ex044.py
+++ Exercicios_41_60/ex044.py
@@ -1,2 +1,28 @@
-''' '''
+'''Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço
+normal e condição de pagamento:
+à vista dinheiro/cheque: 10% de desconto
+à vista no cartão: 5% de desconto
+em até 2x no cartão: preço formal
+3x ou mais no cartão: 20% de juros'''
 
+print("===== LOJAS CALI =====")
+preço = float(input("Preço das compras: R$"))
+print("FORMAS DE PAGAMENTO\n"
+      "[1] à vista no dinheiro\n"
+      "[2] à vista no cartão\n"
+      "[3] 2x no cartão\n"
+      "[4] 3x ou mais no cartão")
+op = int(input("Qual opção: "))
+if op == 1:
+    desconto = preço * 10 / 100
+    print(f"Sua compra de R${preço} vai custar R${preço - desconto} no final da compra.")
+elif op == 2:
+    desconto = preço * 5 / 100
+    print(f"Sua compra de R${preço} vai custar R${preço - desconto} no final no final da compra.")
+elif op == 3:
+    print(f"Sua compra vai custar R${preço} no final da compra")
+elif op == 4:
+    juros = preço * 20 / 100
+    print(f"Sua compra de R${preço} vai custar R${preço + juros} no final no final da compra.")
+else:
+    print("Opção inválida.")
--- Exercicios_41_60/ex045.py
+++ Exercicios_41_60/ex045.py
@@ -1,2 +1,30 @@
-''' '''
+'''Crie um programa que faça o computador jogar Jokenpô com você.'''
 
+from time import sleep
+from random import choice
+
+computador = choice([1, 2, 3])
+print("Suas opções.\n"
+      "[1] PEDRA\n"
+      "[2] PAPEL\n"
+      "[3] TESOURA")
+jogador = int(input("Qual a sua jogada: "))
+print("JO")
+sleep(1)
+print("KEN")
+sleep(1)
+print("PÔ!!!")
+print("-=-" * 10)
+print(f"COMPUTADOR JOGOU {computador}")
+print(f"JOGADOR JOGOU {jogador}")
+print("-=-" * 10)
+if jogador == computador:
+    print("EMPATE!!!")
+elif jogador == 1 and computador == 3:
+    print("Jogador VENCEU!!!")
+elif jogador == 2 and computador == 1:
+    print("Jogador VENCEU!!!")
+elif jogador == 3 and computador == 2:
+    print("Jogador VENCEU!!!")
+else:
+    print("Computador VENCEU!!!")
