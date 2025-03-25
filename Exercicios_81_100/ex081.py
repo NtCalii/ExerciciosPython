4c759c8e3ad529eace336b2703e8ccb1d6336d05
Nathan Caliani <nathan.caliani09@gmail.com>
2025-03-25 17:37:46 -0300

    Ex 81 até 83 adicionados conteúdo

--- Exercicios_81_100/ex081.py
+++ Exercicios_81_100/ex081.py
@@ -1,2 +1,20 @@
-''' '''
+'''Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+A) Quantos números foram digitados.
+B) A lista de valores, ordenada de forma decrescente.
+C) Se o valor 5 foi digitado e está ou não na lista.'''
 
+lista = []
+while True:
+    lista.append(int(input("Digite um valor: ")))
+    continuar = input("Quer continuar? [S/N]: ").upper()
+    if continuar == "N":
+        break
+print("=-=" * 10)
+lista.sort(reverse=True)
+print(f"Você digitou {len(lista)}.\n"
+      f"Os valores em ordem decresente são {lista}\n"
+      f"O valor 5 ", end="")
+if 5 in lista:
+    print("Faz parte da lista." )
+else:
+    print("Não faz parte da lista.")
--- Exercicios_81_100/ex082.py
+++ Exercicios_81_100/ex082.py
@@ -1,2 +1,21 @@
-''' '''
+'''Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas
+extras que vão conter apenas os valores pares e os valores ímpares digitados, respectivamente.
+Ao final, mostre o conteúdo das três listas geradas.'''
 
+lista1 = []
+listapar = []
+listaimpar = []
+while True:
+    lista1.append(int(input("Digite um valor: ")))
+    continuar = input("Quer continuar? [S/N]: ").upper()
+    if continuar == "N":
+        break
+print("=-=" * 10)
+for i, v in enumerate(lista1):
+    if v % 2 == 0:
+        listapar.append(v)
+    else:
+        listaimpar.append(v)
+print(f"A lista completa {lista1}\n"
+      f"A lista par {listapar}\n"
+      f"A lista ímpar {listaimpar}")
