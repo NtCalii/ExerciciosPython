c819d76f31d2cf08cd0ebb19f5eb807b85b90cea
Nathan Caliani <nathan.caliani09@gmail.com>
2025-04-03 17:33:57 -0300

    Ex conteúdo add
    ex 108 conteúdo add em comentários

--- Exercicios_100_15/ex108.py
+++ Exercicios_100_15/ex108.py
@@ -1,2 +1,9 @@
-''' '''
+'''Adapte o código do desafio #107, criando uma função adicional chamada moeda() que consiga mostrar
+os números como um valor monetário formatado.'''
 
+#import moeda
+
+#preco = float(input("Digite um preço: R$"))
+#print(f"A metade desse preço é R${moeda.moedas(moeda.metade(preco)):.2f}")
+#print(f"O dobro de R${preco:.2f} é R${moeda.moedas.dobro(preco):.2f}")
+#print(f"Aumentando 10% fica R${moeda.moedas.aumentar(preco):.2f}")
--- Exercicios_100_15/moeda.py
+++ Exercicios_100_15/moeda.py
@@ -12,3 +12,6 @@ def dobro(n1):
 def metade(n1):
     n1 /= 2
     return n1
+
+#def moedas(preco=0):
+#    return f"{moedas()}{preco:>8.2f}".replace(".", ",")
