4c0da819a1f030d4c92e334e80931f89c239f0c0
Nathan Caliani <nathan.caliani09@gmail.com>
2025-04-03 17:36:12 -0300

    Ex conteúdo add
    ex 109 add em comentários

--- Exercicios_100_15/ex109.py
+++ Exercicios_100_15/ex109.py
@@ -1,2 +1,10 @@
-''' '''
+'''Modifique as funções que form criadas no desafio 107 para que elas aceitem um parâmetro
+a mais, informando se o valor retornado por elas vai ser ou não formatado pela função moeda(),
+desenvolvida no desafio 108.'''
 
+#import moeda
+
+#preco = float(input("Digite um preço: R$"))
+#print(f"A metade desse preço é R${moeda.metade(preco):.2f}")
+#print(f"O dobro de R${preco:.2f} é R${moeda.dobro(preco):.2f}")
+#print(f"Aumentando 10% fica R${moeda.aumentar(preco):.2f}")
