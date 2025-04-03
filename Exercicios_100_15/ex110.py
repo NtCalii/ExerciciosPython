856419a793d5dc1aa108a69a1740cfbecfb59635
Nathan Caliani <nathan.caliani09@gmail.com>
2025-04-03 17:46:32 -0300

    Ex 110 conteúdo add
    ex 110 add junto com resumo em moeda

--- Exercicios_100_15/ex110.py
+++ Exercicios_100_15/ex110.py
@@ -1,2 +1,7 @@
-''' '''
+'''Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
+que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.'''
 
+import moeda
+
+num = int(input("Digite um número: "))
+moeda.resumo(num, 20)
--- Exercicios_100_15/moeda.py
+++ Exercicios_100_15/moeda.py
@@ -1,8 +1,8 @@
 '''Crie um módulo chamado moeda.py que tenha as funções incorporadas aumentar(), dobro()
  e metade(). Faça também um programa que importe esse módulo e use algumas dessas funções.'''
 
-def aumentar(n1):
-    n1 = n1 * 10 / 100
+def aumentar(n1, taxa=0):
+    n1 = n1 * taxa / 100
     return n1
 
 def dobro(n1):
@@ -15,3 +15,12 @@ def metade(n1):
 
 #def moedas(preco=0):
 #    return f"{moedas()}{preco:>8.2f}".replace(".", ",")
+
+def resumo(n1, taxa):
+    print("---------------\n"
+          "RESUMO DO VALOR\n"
+          "---------------")
+    print(f"Preço analisado: {n1}\n"
+          f"Dobro do preço: {dobro(n1)}\n"
+          f"Metade do preço: {metade(n1)}\n"
+          f"{taxa}% de aumento: {aumentar(n1, taxa)}")
