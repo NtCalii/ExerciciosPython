0aea5fbd9611468fd1d6b0416c9938e5e92fefa3
Nathan Caliani <nathan.caliani09@gmail.com>
2025-04-04 17:28:30 -0300

    ex conteúdo add

--- Exercicios_100_15/ex113.py
+++ Exercicios_100_15/ex113.py
@@ -1,2 +1,29 @@
-''' '''
+'''Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade
+da digitação de um número de tipo inválido. Aproveite e crie também uma função leiaReal() com a
+mesma funcionalidade.'''
 
+def leiaInt(msg):
+    while True:
+        try:
+            num_int = int(input(msg))
+        except (ValueError, TypeError, KeyboardInterrupt):
+            print(f"ERRO: Por favor, digite um número inteiro válido.")
+            continue
+        else:
+            break
+    return num_int
+
+def leiaReal(msg):
+    while True:
+        try:
+            num_real = float(input(msg))
+        except (ValueError, TypeError, KeyboardInterrupt):
+            print(f"ERRO: Por favor, digite um número real válido.")
+            continue
+        else:
+            break
+    return num_real
+
+num_int = leiaInt("Digite uma valor inteiro: ")
+num_real = leiaReal("Digite um real: ")
+print(f"O valor inteiro digitado foi {num_int} e o real foi {num_real}")
--- Exercicios_100_15/ex114.py
+++ Exercicios_100_15/ex114.py
@@ -1,2 +1,11 @@
-''' '''
+'''Crie um código em Python que teste se o site pudim está acessível pelo computador usado.'''
 
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen("https://www.youtube.com/")
+except:
+    print("O site youtube não está dísponivel no momento.")
+else:
+    print("Site acessado com sucesso.")
